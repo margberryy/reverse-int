3b29e253c45026370ca9a6b9c561d71228938b35
Marg <paerl.mag@gmail.com>
2021-02-22 19:48:40 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+   n = Math.abs(n)
+   let num = String(n);
+   let arr = Array.from(num);
+   let revertNum = Number(arr.reverse().join(''));
+   return revertNum;
 }
